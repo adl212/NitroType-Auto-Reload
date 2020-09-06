aafbe71e158f7aca866eb3624ba474cfb4b9c0cc
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-09-06 15:30:51 +0000

    Update auto_reload.user.js

--- auto_reload.user.js
+++ auto_reload.user.js
@@ -1,21 +1,16 @@
 // ==UserScript==
 // @name         NitroType Auto Reload
 // @namespace    https://github.com/Ray-Adams
-// @version      1.0.2
+// @version      1.0.3
 // @description  Automatically reloads the page at the end of each race.
 // @author       Ray Adams
 // @match        *://www.nitrotype.com/race
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  'use strict';
-
-  setInterval(function(){
-    var b = document.getElementsByClassName('btn btn--primary btn--fw btn--pulse animate--iconSlam')[0];
-    if (b !== null && b !== undefined) {
-      location.reload(); // Slightly faster than b.click();
-    };
-  }, 100);
-
-})();
+setInterval(() => {
+  let button = document.getElementsByClassName('btn btn--primary btn--fw btn--pulse animate--iconSlam')[0];
+  if (button !== null && button !== undefined) {
+    location.reload(); // Slightly faster than button.click();
+  };
+}, 100);
