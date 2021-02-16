810541a4e218334fea09a880179cf6982cbe8bfb
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2021-02-15 17:47:57 -0800

    v1.1.0

--- auto_reload.user.js
+++ auto_reload.user.js
@@ -1,16 +1,39 @@
 // ==UserScript==
 // @name         NitroType Auto Reload
 // @namespace    https://github.com/Ray-Adams
-// @version      1.0.3
+// @version      1.1.0
 // @description  Automatically reloads the page at the end of each race.
 // @author       Ray Adams
 // @match        *://www.nitrotype.com/race
+// @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
-setInterval(() => {
-  let button = document.getElementsByClassName('btn btn--primary btn--fw btn--pulse animate--iconSlam')[0];
-  if (button !== null && button !== undefined) {
-    location.reload(); // Slightly faster than button.click();
-  };
-}, 100);
+(() => {
+
+    const options = {
+        intervalMs: 100,             // Recommended milliseconds
+        reloadWhenDisqualified: true // Can be changed to false
+    };
+
+    const client = () => {
+
+        let btn = {
+            Dqed: options.reloadWhenDisqualified ? document.getElementsByClassName('btn btn--light btn--fw')[1] : null,
+            Finished: document.getElementsByClassName('btn btn--primary btn--fw btn--gloss animate--iconSlam dhf')[0]
+        };
+
+        if (document.body.contains(btn.Dqed || btn.Finished)) {
+
+            clearInterval(intervalId);
+            location.reload();
+
+        }
+
+    };
+
+    const intervalId = setInterval(client, options.intervalMs);
+
+    console.info('Auto Reload Activated.')
+
+})()
